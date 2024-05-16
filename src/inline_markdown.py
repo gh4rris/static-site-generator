8dfe6fd81b6f75277a416c468f17f85495bd7908
gh4rris <gharris90@hotmail.co.uk>
2024-05-16 23:05:11 +0100

    split image and link nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -4,7 +4,9 @@ from textnode import(
     text_type_text,
     text_type_bold,
     text_type_italic,
-    text_type_code)
+    text_type_code,
+    text_type_link,
+    text_type_image)
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes =[]
@@ -29,5 +31,46 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
 
-text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
-print(extract_markdown_links(text))
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        if extract_markdown_images(node.text) == []:
+            new_nodes.append(node)
+        image_tuples = extract_markdown_images(node.text)
+        original_text = node.text
+        for tuple in image_tuples:
+            split_text = original_text.split(f"![{tuple[0]}]({tuple[1]})", 1)
+            if len(split_text) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], text_type_text))
+            new_nodes.append(TextNode(tuple[0], text_type_image, tuple[1]))
+            original_text = split_text[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+            
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        if extract_markdown_links(node.text) == []:
+            new_nodes.append(node)
+        link_tuples = extract_markdown_links(node.text)
+        original_text = node.text
+        for tuple in link_tuples:
+            split_text = original_text.split(f"[{tuple[0]}]({tuple[1]})", 1)
+            if len(split_text) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], text_type_text))
+            new_nodes.append(TextNode(tuple[0], text_type_link, tuple[1]))
+            original_text = split_text[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+
+
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,13 +1,20 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import(
+    split_nodes_delimiter,
+    extract_markdown_images, 
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link)
 
 from textnode import(
     TextNode,
     text_type_text,
     text_type_bold,
     text_type_italic,
-    text_type_code)
+    text_type_code,
+    text_type_link,
+    text_type_image)
 
 class TestInLineMarkdown(unittest.TestCase):
 
@@ -31,6 +38,7 @@ class TestInLineMarkdown(unittest.TestCase):
                           TextNode(" and so ", text_type_text),
                           TextNode("is this", text_type_bold)], new_nodes)
         
+    
     def test_extract_markdown_images(self):
         text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
         self.assertEqual([("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), 
@@ -41,6 +49,26 @@ class TestInLineMarkdown(unittest.TestCase):
         self.assertEqual([("link", "https://www.example.com"), 
                           ("another", "https://www.example.com/another")], extract_markdown_links(text))
         
+    def test_extract_no_image(self):
+        text = "This is text with no image"
+        self.assertEqual([], extract_markdown_images(text))
+        
+    
+    def test_split_nodes_image(self):
+        node = TextNode("This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)", text_type_text)
+        new_node = split_nodes_image([node])
+        self.assertEqual([TextNode("This is text with an ", text_type_text), 
+                          TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), 
+                          TextNode(" and ", text_type_text), 
+                          TextNode("another", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")], new_node)
+        
+    def test_split_nodes_link(self):
+        node = TextNode("This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)", text_type_text)
+        new_node =split_nodes_link([node])
+        self.assertEqual([TextNode("This is text with a ", text_type_text),
+                          TextNode("link", text_type_link, "https://www.example.com"),
+                          TextNode(" and ", text_type_text),
+                          TextNode("another", text_type_link, "https://www.example.com/another")], new_node)
 
 if __name__ == "__main__":
     unittest.main()
