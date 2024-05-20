550cd819639e19df1e4ab72c65ff5f2425899e2f
gh4rris <gharris90@hotmail.co.uk>
2024-05-20 22:07:08 +0100

    block to block type

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,14 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import(
+    markdown_to_blocks, 
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_unordered_list,
+    block_type_ordered_list)
 
 class TestMarkdownToHTML(unittest.TestCase):
 
@@ -13,3 +21,24 @@ class TestMarkdownToHTML(unittest.TestCase):
         markdown = "This is **bolded** paragraph\n\n\n\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n\n* This is a list\n* with items"
         blocks = markdown_to_blocks(markdown)
         self.assertEqual(["This is **bolded** paragraph", "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line", "* This is a list\n* with items"], blocks)
+
+    
+    def test_block_to_block_type(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote1\n> quote2"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* unordered1\n* unordered2"
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        block = "- unordered1\n- unordered2"
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        block = "1. ordered1\n2. ordered2"
+        self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
+
+
+if __name__ == "__main__":
+    unittest.main()
