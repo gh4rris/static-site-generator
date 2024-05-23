447f78dd57bc21eca00c3c3620c9fc25d3de12b2
gh4rris <gharris90@hotmail.co.uk>
2024-05-23 21:44:55 +0100

    generate pages recursive

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 def generate_page(from_path, template_path, dest_path):
@@ -30,3 +31,13 @@ def extract_title(markdown):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("Invalid markdown: no header")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
